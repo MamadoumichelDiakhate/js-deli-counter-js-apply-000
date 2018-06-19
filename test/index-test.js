6fed3ac420489ef3686e1e4071a28ca8b1308c32
mamadou.m.diakhate@gmail.com <mamadou.m.diakhate@gmail.com>
2018-06-19 03:56:03 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -72,13 +72,16 @@ function nowServing(katzDeli){
 
 /*Build a function currentLine that returns the current line*/
 var line = [];
-function currentLine(katzDeli){
+function currentLine(katzDeli) {
   let i = 0;
   while (i < katzDeli.length){
     line.push(` `+[i+1]+`. `  + katzDeli[i])
     i++;
   }
   if (katzDeli.length === 0){
-    
+    return "The line is currently empty.";
   }
+  else
+  return 
+  
 }
